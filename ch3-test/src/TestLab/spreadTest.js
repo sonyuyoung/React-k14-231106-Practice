20aabdd43b3b57a261f9b54cce81418b24a4b661
lsy3709 <lsy3709@naver.com>
2023-11-07 15:01:36 +0900

    231107 6교시 반복문 출력 형식 수정

--- ch3-test/src/TestLab/spreadTest.js
+++ ch3-test/src/TestLab/spreadTest.js
@@ -1,12 +1,13 @@
 const obj1 = { a: 1, b: 2, c: 3 };
 const nextObj1 = { ...obj1, d: 4 };
-for (const key in obj1) {
+for (const key in nextObj1) {
   console.log("복사본의 값: " + nextObj1[key]);
 }
 // ... spread 연산자를 이용해서, 사본 만들기.
 // 복사본을 만들어서 d값만 덮어쓰기.
 
 console.log("==============================");
+
 // 배열
 const array = [
   { id: 1, value: true },
@@ -18,20 +19,49 @@ const array = [
 let nextArr = array.concat({ id: 4, value: false });
 
 // filter , (item) => item.id !== 2 . 2값 제거
+// 배열에서 각 요소를 하나씩 꺼내서 item에 담아서 확인.
 let nextArr2 = nextArr.filter((item) => item.id !== 2);
 
 //map item.id === 1 -> 이면 value 값 false 값 변경.
+// 배열에서 각 요소를 하나씩 꺼내서 item에 담아서 확인
 let nextArr3 = nextArr.map((item) =>
   item.id === 1 ? { ...item, value: false } : item
 );
 
 // 출력 걸어두기.
+for (let i = 0; i < nextArr.length; i++) {
+  console.log(
+    "concat 확인. nextArr: " +
+      nextArr[i] +
+      "key: " +
+      nextArr[i].id +
+      " , valeu: " +
+      nextArr[i].value
+  );
+}
+console.log("==============================");
+
+// 출력 걸어두기.
 for (let i = 0; i < nextArr2.length; i++) {
-  console.log(nextArr2[i]);
+  console.log(
+    "concat 확인. nextArr2: " +
+      nextArr2[i] +
+      "key: " +
+      nextArr2[i].id +
+      " , valeu: " +
+      nextArr2[i].value
+  );
 }
 
 console.log("==============================");
 // 출력 걸어두기2
 for (let i = 0; i < nextArr3.length; i++) {
-  console.log(nextArr3[i]);
+  console.log(
+    "concat 확인. nextArr3: " +
+      nextArr3[i] +
+      "key: " +
+      nextArr3[i].id +
+      " , valeu: " +
+      nextArr3[i].value
+  );
 }

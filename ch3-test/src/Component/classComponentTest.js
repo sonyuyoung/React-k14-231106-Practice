2bd0c082342917d417c584c63648f6f6779f24de
lsy3709 <lsy3709@naver.com>
2023-11-07 09:51:19 +0900

    231107 1교시 컴포넌트 준비

---
+++ ch3-test/src/Component/classComponentTest.js
@@ -0,0 +1,11 @@
+// 클래스형 컴포넌트 테스트
+// rcc
+import React, { Component } from "react";
+
+class classComponentTest extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default classComponentTest;
---
+++ ch3-test/src/Component/funcComponentTest.js
@@ -0,0 +1,9 @@
+// 함수형 컴포넌트 테스트
+// rsc
+import React from "react";
+
+const funcComponentTest = () => {
+  return <div></div>;
+};
+
+export default funcComponentTest;

58c80b6be023005b9e339e4836e5072c4e7c7d62
lsy3709 <lsy3709@naver.com>
2023-11-07 11:17:39 +0900

    231107 3교시 props 간단 예제 이름 패스워드 전달 및 가져오기

--- ch3-test/src/App.js
+++ ch3-test/src/App.js
@@ -5,6 +5,7 @@ import Test from "./Component/Test";
 import FuncTest1 from "./Component/FuncTest1";
 import ClassTest from "./Component/ClassTest";
 import Test33 from "./Component/Test33";
+import MyComp from "./Component/MyComp";
 
 // 부모 컴포넌트
 function App() {
@@ -16,6 +17,8 @@ function App() {
       <FuncTest1 />
       <ClassTest />
       <Test33 />
+      {/* 자식 컴포넌트에게 props 라는 속성 객체를 이용해서 이름을 전달하기 */}
+      <MyComp name="이상용" password="1234" />
     </>
   );
 }

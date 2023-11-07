6ced5c3241f3fe85e965c3671ea18ddecf7c9610
lsy3709 <lsy3709@naver.com>
2023-11-07 14:29:20 +0900

    231107 5교시  색깔 버튼으로 메세지 변경 확인.

--- ch3-test/src/Component/MySay.js
+++ ch3-test/src/Component/MySay.js
@@ -8,17 +8,30 @@ const MySay = () => {
   // const [상태값,세터함수] = useState('')-> 반환 배열 형식.
   const [username, setUserName] = useState("lsy");
   const [password, setPassword] = useState("1234");
+
+  // 색깔 추가 해보기.
+  const [color, setColor] = useState("red");
+
   // 이벤트 핸들러 추가. onClick , 클릭시 이벤트 발생.
   const onClickUsername = () => setUserName("username 더미값");
   const onClickPassword = () => setPassword("password 더미값");
   return (
     <div>
-      <h1>{username} 님 환영합니다.</h1>
+      <h1 style={{ color }}>{username} 님 환영합니다.</h1>
       <h1>
         {username} 님 패스워드 확인 테스트: {password}{" "}
       </h1>
       <button onClick={onClickUsername}>username 확인하기</button>
       <button onClick={onClickPassword}>password 확인하기</button>
+      <button style={{ color: "red" }} onClick={() => setColor("red")}>
+        빨간색꾸미기
+      </button>
+      <button style={{ color: "blue" }} onClick={() => setColor("blue")}>
+        파란색꾸미기
+      </button>
+      <button style={{ color: "green" }} onClick={() => setColor("green")}>
+        초록색꾸미기
+      </button>
     </div>
   );
 };

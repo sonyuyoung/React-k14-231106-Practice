8155ff710c3995ec4df87abb145ddbf128000094
lsy3709 <lsy3709@naver.com>
2023-11-07 16:10:55 +0900

    231107 7교시 이벤트 핸들러 뼈대2, form 방식

--- ch4-test/src/component/Join.js
+++ ch4-test/src/component/Join.js
@@ -3,23 +3,59 @@ import React from "react";
 import { useState } from "react";
 
 const Join = () => {
+  // 방법1
   //useState 이용해서, 현재 상태값 , 세터 함수 정의하기.
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+
+  //방법2  , 특정 객체에 담아서(박스(모델)에 담아서)
+  // 초깃값으로 객체를 선언 했음.
+  // form 상태값에, 객체가 할당이 됨.
+  // 앞에 부분과 비교하면, 앞에 개별적으로 처리했고, 객체에 담아서 일괄처리.
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  // 비구조화 할당으로
+  const { email, password } = form;
 
   // onChange, onClick , onKeyPress 등 확인.
   // input 입력된 값의 변경 유무를 확인.
-  const onChangeEmail = (e) => setEmail(e.target.value);
+  // 방법1
+  // const onChangeEmail = (e) => setEmail(e.target.value);
+
+  // 방법2 , form 내부에 값이 변경시 처리하는 로직.
+  const onChangeForm = (e) => {
+    const nextForm = {
+      ...form,
+      // email input -> name : email , value : 실제 입력된 값
+      // password input -> name : email , value : 실제 입력된 값
+      [e.target.name]: e.target.value,
+    };
+    setForm(nextForm);
+  };
 
   // 추가 패스워드 변경 부분, 해당 뷰에 나타내기 테스트. -> 나중에 데이터 전달
   // 통신 라이브러리 사용함, axios 사용할 예정. 파이어베이스를 연동부분. 할예정.
-  const onChangePassword = (e) => setPassword(e.target.value);
+  // 방법1
+  // const onChangePassword = (e) => setPassword(e.target.value);
 
   // 클릭 이벤트 발생시 값을 확인.
+  // 방법1
+  // const onClick = () => {
+  //   alert("email: " + email + ", password : " + password);
+  //   setEmail("");
+  //   setPassword("");
+  // };
+
+  // 방법2. form 객체에 담아서 일괄 처리. 하는중.
   const onClick = () => {
     alert("email: " + email + ", password : " + password);
-    setEmail("");
-    setPassword("");
+    setForm({
+      email: "",
+      password: "",
+    });
   };
 
   // 키보드에서 엔터 키 입력시, 클릭 이벤트 호출 연결 확인.
@@ -39,7 +75,10 @@ const Join = () => {
         name="email"
         placeholder="이메일 입력해주세요."
         value={email}
-        onChange={onChangeEmail}
+        // 방법1
+        // onChange={onChangeEmail}
+        // 방법2
+        onChange={onChangeForm}
         onKeyPress={onKeyPress}
       />
       <br />
@@ -48,7 +87,10 @@ const Join = () => {
         name="password"
         placeholder="패스워드를 입력해주세요."
         value={password}
-        onChange={onChangePassword}
+        // 방법1
+        // onChange={onChangePassword}
+        // 방법2
+        onChange={onChangeForm}
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>

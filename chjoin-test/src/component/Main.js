e6ff78bf52aaf5b14f61aa9a42fc9813b10bfc49
lsy3709 <lsy3709@naver.com>
2023-11-08 12:40:14 +0900

    231108 4교시 styled-components 사용해보기

--- chjoin-test/src/component/Main.js
+++ chjoin-test/src/component/Main.js
@@ -9,37 +9,94 @@ import { Button } from "antd";
 
 // styled-component,  컴포넌트에서 좀더 편하게 css  작업하는 모듈.
 // yarn add styled-components
+// npm install styled-components
 // 확장팩: styled-components
+// 설치 후, 모듈 가져와서 사용하기.
+// 공식 문서
+// https://styled-components.com/docs/basics#getting-started
+import styled from "styled-components";
+import { Component } from "react";
+
+// styled-component 사용해보기
+// 예제
+// styled.원하는태그(적용하고 싶은태그) 선택 `(백틱)   `(백틱),열고 닫고
+// 이 사이에 css 문법을 작성.
+const MainTitleTextCss = styled.p`
+  font-size: 30px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const MainTextCss = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  background-color: red;
+  text-align: center;
+`;
+
+//Wrapper 라고 해서 블록 부분 설정.
+const Wrapper = styled.div`
+  padding: 20px;
+  width: calc(100% - 40px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+// Container 만들기.
+const Container = styled.div`
+  width: 100%;
+  max-width: 720px;
+  margin-left: 20px;
+
+  // & : 현재 태그
+  // div 태그 하위의 자식 태그를 마지막 자식 태그를 제외하고
+  // 각 요소의 마진 바텀을 16 px 씩 간격을 주겠다.
+  & {
+    :not(:last-child) {
+      margin-bottom: 16px;
+    }
+  }
+`;
 
 const Main = () => {
   // useNavigate 라는 훅스를 이용해서, 페이징 하기..
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>메인 화면입니다.</h1>
-      {/* join 컴포넌트로 이동하는 버튼 하나 추가 */}
-      <Button
-        title="회원가입 이동"
-        type="primary"
-        onClick={() => {
-          navigate("/join");
-        }}
-      >
-        회원가입 이동
-      </Button>
-      <br />
-      <br />
-      <Button
-        title="mycount 이동"
-        type="primary"
-        onClick={() => {
-          navigate("/mycount");
-        }}
-      >
-        mycount 이동
-      </Button>
-    </div>
+    <Wrapper>
+      <div>
+        <h1>메인 화면입니다.</h1>
+        {/* css 적용하기 */}
+        <Container>
+          <MainTitleTextCss>styled-components test</MainTitleTextCss>
+
+          <MainTextCss>2번째 텍스트 효과 확인하기.</MainTextCss>
+          {/* join 컴포넌트로 이동하는 버튼 하나 추가 */}
+          <Button
+            title="회원가입 이동"
+            type="primary"
+            onClick={() => {
+              navigate("/join");
+            }}
+          >
+            회원가입 이동
+          </Button>
+          <br />
+          <br />
+          <Button
+            title="mycount 이동"
+            type="primary"
+            onClick={() => {
+              navigate("/mycount");
+            }}
+          >
+            mycount 이동
+          </Button>
+        </Container>
+      </div>
+    </Wrapper>
   );
 };
 

a935150796be3790fc70439f84527f039d518cb5
lsy3709 <lsy3709@naver.com>
2023-11-08 11:38:48 +0900

    231108 3교시 코드 증발 복구

---
+++ chjoin-test/src/component/Main.js
@@ -0,0 +1,23 @@
+// Main 임시 페이지
+import React from "react";
+// yarn add react-router-dom
+// npm install react-router-dom
+// 라우팅 모듈 , 도구 설치.
+// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Button } from "antd";
+
+const Main = () => {
+  // useNavigate 라는 훅스를 이용해서, 페이징 하기..
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>메인 화면입니다.</h1>
+      {/* join 컴포넌트로 이동하는 버튼 하나 추가 */}
+      <Button title="회원가입 이동" onClick={navigate("/join")} />
+    </div>
+  );
+};
+
+export default Main;

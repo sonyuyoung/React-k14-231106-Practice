712ef59f01b2de2da79f9e92475208eae235e55e
lsy3709 <lsy3709@naver.com>
2023-11-08 11:34:51 +0900

    231108 3교시 기본 라우팅 2

--- chjoin-test/src/App copy.js
+++ chjoin-test/src/App copy.js
@@ -20,6 +20,32 @@ function App() {
         {/* 주소: http://localhost:3000/join -> 해당 페이지 안내 : element={<이동할 컴포넌트>} */}
         <Route path="join" element={<Join />} />
       </Routes>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* antd 튜토리얼 샘플 가져오기 테스트 */}
+          <div style={{ padding: "0 24px" }}>
+            <h1>antd version: {version}</h1>
+            <Space>
+              <DatePicker />
+              <Button type="primary">Primary Button</Button>
+            </Space>
+          </div>
+          {/* antd 튜토리얼 샘플 가져오기 테스트 */}
+          <Join />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </BrowserRouter>
   );
 }

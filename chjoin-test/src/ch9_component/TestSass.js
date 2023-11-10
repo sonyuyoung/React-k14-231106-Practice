e7e6e3359d650d621c0c23dc5dc876b9cc9163cb
lsy3709 <lsy3709@naver.com>
2023-11-10 12:12:56 +0900

    1110 4교시 CSS Module 준비, 클래스 이름 중복 방지, 확인

--- chjoin-test/src/ch9_component/TestSass.js
+++ chjoin-test/src/ch9_component/TestSass.js
@@ -1,15 +1,24 @@
 //방금 만든 css 문법을 테스트할 빈도화지
 
+// CSS Modul 테스트 해보기.
+// 만든 CSSModule.module.css 불러오기
+import cssmodule from "./CSSModule.module.css";
+
 import React from "react";
 import "./SassComponent.scss";
 
 const TestSass = () => {
   return (
-    <div className="SassTest">
-      <div className="box red"></div>
-      <div className="box blue"></div>
-      <div className="box green"></div>
-    </div>
+    <>
+      {/* CSS Modul 테스트 해보기. 적용하기. */}
+      <div className={cssmodule.wrapper}>CSS Modul 테스트 해보기</div>
+      <div className="testGlobal">CSS Modul testGlobal 테스트 해보기2</div>
+      <div className="SassTest">
+        <div className="box red"></div>
+        <div className="box blue"></div>
+        <div className="box green"></div>
+      </div>
+    </>
   );
 };
 

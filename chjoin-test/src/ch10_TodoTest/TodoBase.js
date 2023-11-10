19e282d81e3699e2b7ec9d2daa537b3735dd8560
lsy3709 <lsy3709@naver.com>
2023-11-10 15:13:28 +0900

    1110 6교시 일정관리 준비 작업 1, 기본 뼈대3 ,TodoBase css작업중.

--- chjoin-test/src/ch10_TodoTest/TodoBase.js
+++ chjoin-test/src/ch10_TodoTest/TodoBase.js
@@ -5,9 +5,26 @@ import styled from "styled-components";
 // AppTitle
 // Content
 
-const TodoBaseTemp = styled.div``;
-const AppTitle = styled.div``;
-const Content = styled.div``;
+const TodoBaseTemp = styled.div`
+  width: 512px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 6rem;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+const AppTitle = styled.div`
+  background: green;
+  color: black;
+  height: 4rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Content = styled.div`
+  background: white;
+`;
 
 const TodoBase = ({ children }) => {
   return (

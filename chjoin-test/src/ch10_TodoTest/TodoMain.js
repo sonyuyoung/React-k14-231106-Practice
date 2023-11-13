8c0c7b2f6895e6bbebd37f97fc404ea456047a11
lsy3709 <lsy3709@naver.com>
2023-11-13 10:23:37 +0900

    2교시 TodoMain , 임시 데이터 배열 만들어서, TodoList 컴포넌트 props 속성으로 데이터 전달.

--- chjoin-test/src/ch10_TodoTest/TodoMain.js
+++ chjoin-test/src/ch10_TodoTest/TodoMain.js
@@ -3,12 +3,13 @@
 // 전체 가운데 요소로 정렬 시켜주는 템플릿 :TodoBase
 // 1)제목 2) 입력란:TodoInsert 3) 리스트 4) 리스트의 아이템 등.
 
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { AiFillApple } from "react-icons/ai";
+// import { AiFillApple } from "react-icons/ai";
 import TodoBase from "./TodoBase";
 import TodoInsert from "./TodoInsert";
-import TodoListItem from "./TodoListItem";
+
+import TodoList from "./TodoList";
 
 const Main_css = styled.div`
   margin: 0;
@@ -19,11 +20,31 @@ const Main_css = styled.div`
 
 // Todo 만들기 준비 메인 <AiFillApple />
 const TodoMain = () => {
+  //샘플 더미 데이터를 임시 배열에 만들어서, 전달. props 테스트
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "더미 데이터 요소 1번입니다.",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "더미 데이터 요소 2번입니다.",
+      checked: true,
+    },
+    {
+      id: 1,
+      text: "더미 데이터 요소 3번입니다.",
+      checked: true,
+    },
+  ]);
+
   return (
     <Main_css>
       <TodoBase>
         <TodoInsert />
-        <TodoListItem />
+        {/* 위에서 만든 임시 데이터 배열를 전달 : props 속성으로 전달 */}
+        <TodoList todos={todos} />
       </TodoBase>
     </Main_css>
   );

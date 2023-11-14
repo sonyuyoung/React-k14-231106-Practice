d71c3c4a5b253390c83d5674edcd5334695d54b7
lsy3709 <lsy3709@naver.com>
2023-11-14 12:45:15 +0900

    4교시 axios 기본 get 이용해서, 데이터 하나 받아오기 , async , await 버전으로 변경.

--- chjoin-test/src/ch13_API_PublicDataTest/ApiTest.jsx
+++ chjoin-test/src/ch13_API_PublicDataTest/ApiTest.jsx
@@ -10,13 +10,29 @@ const ApiTest = () => {
   // REST API 서버에서, 임시로 받아온 data , state 로 확인하기.
   const [data, setData] = useState(null);
   // 이벤트 핸들러
-  const onClick = () => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos/1")
-      // 위에 get 함수로 데이터를 받는 것 성공했다면, then 이 실행이 됨.
-      .then((response) => {
-        setData(response.data);
-      });
+  // 기존 방식
+  // 방법1
+  // const onClick = () => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/todos/1")
+  //     // 위에 get 함수로 데이터를 받는 것 성공했다면, then 이 실행이 됨.
+  //     .then((response) => {
+  //       setData(response.data);
+  //     });
+  // };
+
+  // 방법2 , async, await 버전으로 변경.
+  // 비동기 함수로 구현할 함수앞에 키워드 : async 붙이기.
+  const onClick = async () => {
+    try {
+      // await 는 Promise 를 반환하는 함수 앞에 사용
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <div>

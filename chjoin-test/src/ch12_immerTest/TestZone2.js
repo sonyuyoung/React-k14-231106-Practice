030b15ba8b233cbce90dbf3cbb81174be60a135e
lsy3709 <lsy3709@naver.com>
2023-11-14 10:40:50 +0900

    2교시 불변성 테스트 기본 준비 작업 3. immer 기본 사용예제 추가,수정,삭제 splice 소개 확인2

--- chjoin-test/src/ch12_immerTest/TestZone2.js
+++ chjoin-test/src/ch12_immerTest/TestZone2.js
@@ -14,15 +14,49 @@ const TestZone2 = () => {
     },
     end: 5,
   };
+
+  const sampleData = [{ id: 1, name: "lsy", age: "22" }];
   // 기본 문법 살펴보기.
   // produce ( 원본 데이터, 변경할 업데이트 함수)
   const nextState = produce(array, (draft) => {
     // 변경하고 싶은 값 변경하기.
     draft.class.deep404.inside = 30000;
   });
+
+  // 데이터 추가
+  const nextState2 = produce(array, (draft) => {
+    draft.class = { name: "lsy" };
+  });
+
+  //데이터 추가 2
+  const nextState3 = produce(sampleData, (draft) => {
+    draft.push({ id: 2, name: "lsy2", age: "33" });
+  });
+
+  //데이터 제거 1, 제거한 원소를 가지는 배열을 출력.
+  const nextState4 = produce(sampleData, (draft) => {
+    draft.filter((item) => item.id !== 1);
+  });
+
+  //splice 함수 소개.
+  // splice(startIndex,deletecount, newItem, newItem,...)
+  // splice(1,1) : 1이라는 인덱스부터, 1개 요소를 삭제를 하는 내장 함수.
+  //데이터 제거 1, 제거한 원소를 가지는 배열을 출력.
+  const nextState5 = produce(sampleData, (draft) => {
+    draft.splice(
+      draft.findIndex((t) => t.id === 1),
+      1
+    );
+  });
+
   console.log("불변성 유지 하면서 업데이트하기");
   console.log("nextState.class.deep404.inside 의 값: ");
   console.log(nextState.class.deep404.inside);
+  console.log(nextState);
+  console.log(nextState2);
+  console.log(nextState3);
+  console.log(nextState4);
+  console.log(nextState5);
   return (
     <div>
       <h1>결과 뷰: {nextState.class.deep404.inside}</h1>

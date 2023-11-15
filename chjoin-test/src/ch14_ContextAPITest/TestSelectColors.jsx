188007fc464edb09463e9c851b2be50d7981d53c
lsy3709 <lsy3709@naver.com>
2023-11-15 14:03:54 +0900

    5교시 전역으로 속성 값 설정 및 가져오기 테스트3, 함수로 전달하기2 준비2 이벤트핸들러 추가

--- chjoin-test/src/ch14_ContextAPITest/TestSelectColors.jsx
+++ chjoin-test/src/ch14_ContextAPITest/TestSelectColors.jsx
@@ -1,23 +1,45 @@
 import React from "react";
 const colors = ["red", "orange", "blue", "indigo", "violet", "aqua", "navy"];
 
+//전역의 설정 값 또는 함수를 사용하기.
+// Consumer = 게터, Provider = 세터  , 비슷한 역할을 한다.
+import { ColorConsumer } from "./testColor";
+
 const TestSelectColors = () => {
   return (
     <div>
       <h2>색상 선택하기</h2>
-      <div style={{ display: "flex" }}>
-        {colors.map((color) => (
-          <div
-            key={color}
-            style={{
-              background: color,
-              width: "24px",
-              height: "24px",
-              cursor: "pointer",
-            }}
-          />
-        ))}
-      </div>
+      {/* 적용하기, 액션을 가져와서 이용하기. */}
+      <ColorConsumer>
+        {
+          //시작
+          ({ actions }) => (
+            <div style={{ display: "flex" }}>
+              {colors.map((color) => (
+                <div
+                  key={color}
+                  style={{
+                    background: color,
+                    width: "24px",
+                    height: "24px",
+                    cursor: "pointer",
+                  }}
+                  //이벤트 핸들러 추가하기, 전역의 함수를 사용하기.
+                  onClick={() => actions.setColor(color)}
+                  // 우클릭 이벤트 핸들러 추가하기.
+                  onContextMenu={(e) => {
+                    // 우클릭 메뉴가 뜨는 게 기본인데, 기본 동작을 막음.
+                    e.preventDefault();
+                    actions.setSubcolor(color);
+                  }}
+                />
+              ))}
+            </div>
+          )
+          // 함수 끝부분
+        }
+        {/* 끝 */}
+      </ColorConsumer>
       <hr />
     </div>
   );

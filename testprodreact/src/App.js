dadb107cecaae9a4f950ab416ee01f9df2c13eef
02 <sonyuyoung@naver.com>
2023-11-16 10:25:00 +0900

    커밋!

--- testprodreact/src/App.js
+++ testprodreact/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Space, DatePicker, version } from "antd";
+// 페이지 이동을 위한 설정 1
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Main />} />
+
+          <Route path="/mainBook" element={<MainBooks />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

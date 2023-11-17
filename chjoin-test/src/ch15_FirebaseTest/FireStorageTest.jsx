21ab81211c2d456f3cd4a42d7a0960e8b39665b4
lsy3709 <lsy3709@naver.com>
2023-11-17 12:43:38 +0900

    4교시 리액트 스토리지 업로드 확인.

---
+++ chjoin-test/src/ch15_FirebaseTest/FireStorageTest.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+// 스토리지 공식 문서 , 샘플 코드 그대로 복사했고
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { storage } from "./firebaseConfig";
+// const storage = getStorage();
+// storage: 파이어베이스 스토리지에 접근하기 위한 도구, 포인터, 인스턴스.
+// 스토리지 저장소에 , 특정 경로를 만들기.
+// reactStorage/
+
+// 파일 선택하는 input , 보여주기 등.
+const FireStorageTest = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const storageRef = ref(storage, "reactStorage/");
+    // 'file' comes from the Blob or File API
+    const files = event.target.files;
+    const thisFile = files[0];
+    uploadBytes(storageRef, thisFile).then((snapshot) => {
+      console.log("Uploaded a blob or file!");
+    });
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="file" accept="images/*" />
+        <input type="submit" value="Upload" />
+      </form>
+    </div>
+  );
+};
+
+export default FireStorageTest;

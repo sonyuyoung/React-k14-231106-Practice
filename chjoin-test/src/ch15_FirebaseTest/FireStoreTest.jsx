138fe7aa657e557c44c98639afdd2114ab8d4fc5
lsy3709 <lsy3709@naver.com>
2023-11-17 11:48:41 +0900

    3교시 리액트 파이어베이스 getDoc 확인 함수이름 변경

--- chjoin-test/src/ch15_FirebaseTest/FireStoreTest.jsx
+++ chjoin-test/src/ch15_FirebaseTest/FireStoreTest.jsx
@@ -36,7 +36,7 @@ const FireStoreTest = () => {
   };
 
   // 데이터 가져오기. 샘플
-  const getDoc = async () => {
+  const testGetDoc = async () => {
     // db : 공통 도구,
     // cities : 컬렉션에 있는
     // LA 문서를 가져올 예정.
@@ -61,7 +61,7 @@ const FireStoreTest = () => {
         Test setDoc
       </Button>
       &nbsp;&nbsp;
-      <Button type="primary" onClick={() => getDoc()}>
+      <Button type="primary" onClick={() => testGetDoc()}>
         Test getDoc
       </Button>
     </div>
